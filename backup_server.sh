5123ea0eacb38e10462efcb9534870e2ad9c525b
jonizaguirre09 <jonizaguirre09@gmail.com>
2023-11-15 19:04:19 +0100

    labo6 bien

--- backup_server.sh
+++ backup_server.sh
@@ -10,6 +10,6 @@ fecha_ayer=$(date -d "yesterday" +%Y-%m-%d)
 #mkdir -p "/var/tmp/Backups/$fecha_actual"
 
 # Realiza la copia de seguridad incremental al directorio creado
-rsync -av -e ssh --link-dest="jonizaguirre09@34.116.206.18:/var/tmp/Backups/$fecha_ayer" /home/jon/segurtasuna/ "jonizaguirre09@34.116.206.18:/var/tmp/Backups/$fecha_actual/"
+rsync -av -e ssh --link-dest="/var/tmp/Backups/$fecha_ayer" /home/jon/segurtasuna/ "jonizaguirre09@34.116.206.18:/var/tmp/Backups/$fecha_actual/"
 
 #crontab -e
